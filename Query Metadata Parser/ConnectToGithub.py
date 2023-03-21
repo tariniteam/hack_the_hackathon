c658c767e9720948045e26c46ff20f5a38e88db0
tariniteam <tariniteam@gmail.com>
2023-03-21 17:22:04 +0530

    Connect to Github module

--- Query Metadata Parser/ConnectToGithub.py
+++ Query Metadata Parser/ConnectToGithub.py
@@ -5,19 +5,28 @@ import lxml
 
 class ConnectToGithub():
 
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, base_github_folder):
+        self.base_github_folder = base_github_folder
 
     def get_sql_scripts(self):
+        print (self.base_github_folder)
 
+    def read_sql_scripts(self):
+        html_response = requests.get(url=self.url)
+        html_data = html_response.text
+        soup = BeautifulSoup(html_data, 'html.parser')
+        print('SQL Scripts')
+        table = soup.find_all('table')
+        df = pd.read_html(str(table))[0]
+        print(df)
+        sql_scripts = df.iloc[:, 1].tolist()
         return sql_scripts
 
 if __name__ == "__main__":
-    url = 'https://github.com/tariniteam/hack_the_hackathon/tree/main/Query%20Metadata%20Parser/SQL%20Scripts'
-    objConnectToGithub = ConnectToGithub(url)
-    sql_scripts = objConnectToGithub.get_sql_keyword_list()
+    base_github_folder = 'https://github.com/tariniteam/hack_the_hackathon/tree/main/Query%20Metadata%20Parser/SQL%20Scripts'
+    url = 'https://github.com/tariniteam/hack_the_hackathon/blob/main/Query%20Metadata%20Parser/SQL%20Scripts/SQL%20Script%201.sql'
+    objConnectToGithub = ConnectToGithub(base_github_folder)
+    sql_scripts = objConnectToGithub.read_sql_scripts()
+    print (sql_scripts)
 
 
-
-#with open("myfile.csv", "w") as f:
- #   f.write(resp.text)
--- Query Metadata Parser/Test.py
+++ Query Metadata Parser/Test.py
@@ -1,1 +1,6 @@
-## Test File
+import requests
+
+url = "https://github.com/tariniteam/hack_the_hackathon/blob/main/Query%20Metadata%20Parser/SQL%20Scripts/SQL%20Script%201.sql"
+resp = requests.get(url)
+with open("C:/Users/harsha/OneDrive/Desktop/HARSHA DATA/Cloned Repo/Projects/Query Metadata Parser/myfile.sql", "w", encoding="utf-8") as f:
+    f.write(resp.text)
